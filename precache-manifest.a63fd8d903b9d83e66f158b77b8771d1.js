7976daa503fb9e1ced3d54cacd925b88fd7fdca7
Mateusz Szweda <szweda.mateusz@wp.pl>
2021-12-15 00:11:01 +0000

    Updates

---
+++ precache-manifest.a63fd8d903b9d83e66f158b77b8771d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e72c0a2afc9004d4319e",
+    "url": "/mate966/todo-list/static/js/main.e72c0a2a.chunk.js"
+  },
+  {
+    "revision": "aaceb2a7d185139361cb",
+    "url": "/mate966/todo-list/static/js/1.aaceb2a7.chunk.js"
+  },
+  {
+    "revision": "8c45fbcff94520b7b51d",
+    "url": "/mate966/todo-list/static/js/runtime~main.8c45fbcf.js"
+  },
+  {
+    "revision": "d9320be6b5f1acb79092e558815b15d1",
+    "url": "/mate966/todo-list/static/media/calendar.d9320be6.jpg"
+  },
+  {
+    "revision": "cb5c2a3fd76b40196d0cf224cf0b44ce",
+    "url": "/mate966/todo-list/static/media/calendar-mobile.cb5c2a3f.jpg"
+  },
+  {
+    "revision": "924041bae5660c7121442b7f29801c17",
+    "url": "/mate966/todo-list/static/media/styled-logo.924041ba.png"
+  },
+  {
+    "revision": "de57c30da8cf462a8d0d2337934d082c",
+    "url": "/mate966/todo-list/index.html"
+  }
+];
